2936a9525e5800857e016a021d73f34a0b57cd68
Arsenty Halimovsky <ghost12@yandex.ru>
2024-09-30 03:39:13 +0300

    add comments

--- ToDoJson/ToDoJson/Modules/MainScreenModule/Common/Category.swift
+++ ToDoJson/ToDoJson/Modules/MainScreenModule/Common/Category.swift
@@ -2,7 +2,13 @@ import Foundation
 
 /// Category of selected tasks
 enum Category {
+
+	/// All existent todos
 	case all
+
+	/// Open todos
 	case uncompleted
+
+	/// Closed todos
 	case completed
 }
