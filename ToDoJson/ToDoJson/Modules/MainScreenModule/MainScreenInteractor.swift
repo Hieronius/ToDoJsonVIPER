10063053255b0d81318cfe0baef407b35384fa2e
Arsenty Halimovsky <ghost12@yandex.ru>
2024-10-07 08:49:12 +0300

    Provide documentation for MainScreenInteractor

--- ToDoJson/ToDoJson/Modules/MainScreenModule/MainScreenInteractor.swift
+++ ToDoJson/ToDoJson/Modules/MainScreenModule/MainScreenInteractor.swift
@@ -1,15 +1,34 @@
 import Foundation
 
 /// `Interactor` of the MainScreen with tasks
+/// It is responsible for handling business logic related to tasks, including fetching,
+/// parsing, and filtering tasks. The interactor communicates with the service layer
+/// to retrieve data and informs the presenter of any updates.
 final class MainScreenInteractor: MainScreenInteractorProtocol {
+
+	// MARK: - Public Properties
+
+	/// A weak reference to the presenter
 	weak var presenter: MainScreenPresenter?
-	
+
+	// MARK: - Private Properties
+
 	private let todoService: ToDoService
-	
+
+	// MARK: - Initialization
+
+	/// Initializes the interactor with a given `ToDoService`.
+	///
+	/// - Parameter todoService: An instance of `ToDoService` used for fetching tasks.
 	init(todoService: ToDoService) {
 		self.todoService = todoService
 	}
-	
+
+	// MARK: - Public Methods
+
+	/// Parses tasks fetched from the service and returns them via a completion handler.
+	///
+	/// - Parameter completion: A closure that takes an array of `ToDo` objects as its parameter.
 	func parseTasks(completion: @escaping ([ToDo]) -> Void) {
 		todoService.fetchTasks { result in
 			switch result {
@@ -22,7 +41,13 @@ final class MainScreenInteractor: MainScreenInteractorProtocol {
 			}
 		}
 	}
-	
+
+	/// Loads tasks either from Core Data or by fetching them from a remote source.
+	///
+	/// This method checks if there are existing tasks in Core Data. If there are no tasks,
+	/// it fetches tasks from a remote source using `parseTasks`. Once fetched, it creates
+	/// new task objects in Core Data and informs the presenter to display these tasks.
+	/// If tasks are found in Core Data, it directly informs the presenter with those tasks.
 	func loadTasks() {
 		DispatchQueue.global(qos: .utility).async { [weak self] in
 			guard let self else { return }
@@ -50,7 +75,14 @@ final class MainScreenInteractor: MainScreenInteractorProtocol {
 			}
 		}
 	}
-	
+
+	/// Filters tasks based on the specified category.
+	///
+	/// This method returns an array of `ToDo` objects filtered by the given category.
+	/// It handles three categories: all, completed, and uncompleted.
+	///
+	/// - Parameter category: The `Category` to filter tasks by.
+	/// - Returns: An array of `ToDo` objects matching the specified category.
 	func filterTasks(by category: Category) -> [ToDo] {
 		switch category {
 		case .all:
