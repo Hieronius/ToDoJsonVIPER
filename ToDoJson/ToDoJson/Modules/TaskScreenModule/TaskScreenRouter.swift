743b2a42a9b42e248964b4d589acd1eda4b06fac
Arsenty Halimovsky <ghost12@yandex.ru>
2024-10-07 09:07:33 +0300

    Add documentation to TaskScreenRouter

--- ToDoJson/ToDoJson/Modules/TaskScreenModule/TaskScreenRouter.swift
+++ ToDoJson/ToDoJson/Modules/TaskScreenModule/TaskScreenRouter.swift
@@ -1,9 +1,19 @@
 import UIKit
 
-/// `Router` TaskScreen
+/// `Router` of the Task to manage it's building process and navigation
 final class TaskScreenRouter: TaskScreenRouterProtocol {
+
+	// MARK: - Public Properties
+
+	/// Weak reference to the View to directly point that it's not "owns" it
 	weak var viewController: UIViewController?
-	
+
+	// MARK: - Public Methods
+
+	/// Method build the `TaskScreen` module with dependencies it's need to function correctly
+	/// It sets up the relationships between these components by assigning their properties
+	///
+	/// - Returns: A fully configured `UIViewController` instance representing the TaskScreen
 	static func createModule(with task: ToDo? = nil) -> UIViewController {
 		let view = TaskScreenViewController()
 		let presenter = TaskScreenPresenter()
@@ -20,7 +30,10 @@ final class TaskScreenRouter: TaskScreenRouterProtocol {
 		
 		return view
 	}
-	
+
+	/// Navigates back to the main screen.
+	///
+	/// This method pops the current view controller from the navigation stack
 	func navigateBackToMain() {
 		viewController?.navigationController?.popViewController(animated: true)
 	}
