210f9f2fd0c5fda30f1fd74bc612b50d303e363d
Arsenty Halimovsky <ghost12@yandex.ru>
2024-10-18 03:54:01 +0300

    add documentation

--- ToDoJson/ToDoJson/Helpers/UIView+AutoresizingMask.swift
+++ ToDoJson/ToDoJson/Helpers/UIView+AutoresizingMask.swift
@@ -1,6 +1,8 @@
 import UIKit
 
 extension UIView {
+
+	/// Helper function to wrap TAMIC set to false for an array of `UIView`
 	func disableAutoresizingMask(for views: [UIView]) {
 		views.forEach { $0.translatesAutoresizingMaskIntoConstraints = false }
 	}
