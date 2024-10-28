b52ca3a4b8b543206cec2d75d495a584ee73f46d
Arsenty Halimovsky <ghost12@yandex.ru>
2024-10-28 03:55:58 +0300

    small fix

--- ToDoJson/ToDoJson/Helpers/UIStackView+ArrangedSubviews.swift
+++ ToDoJson/ToDoJson/Helpers/UIStackView+ArrangedSubviews.swift
@@ -5,8 +5,6 @@ extension UIStackView {
 	/// Adds multiple arranged subviews to the stack view.
 	/// - Parameter views: An array of views to be added as arranged subviews.
 	func addArrangedSubviews(_ views: [UIView]) {
-		for view in views {
-			self.addArrangedSubview(view)
-		}
+		views.forEach { self.addArrangedSubview($0) }
 	}
 }
